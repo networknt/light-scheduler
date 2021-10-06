b8fd5699c508f49a469936af0f135cc3d350914a
Steve Hu <stevehu@gmail.com>
2021-10-06 12:19:02 -0400

    fixes #39 Set uncaught exception handler for the kafka streams to log error and replace thread (#40)

--- src/main/java/com/networknt/scheduler/SchedulerStreams.java
+++ src/main/java/com/networknt/scheduler/SchedulerStreams.java
@@ -5,6 +5,9 @@ import com.networknt.kafka.common.KafkaStreamsConfig;
 import com.networknt.kafka.streams.LightStreams;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.errors.DeserializationExceptionHandler;
+import org.apache.kafka.streams.errors.ProductionExceptionHandler;
+import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +25,11 @@ public class SchedulerStreams implements LightStreams {
         streamsProps.putAll(config.getProperties());
         streamsProps.put(StreamsConfig.APPLICATION_SERVER_CONFIG, ip + ":" + port);
         schedulerStreams = new KafkaStreams(topology.buildTaskStreamingTopology(), streamsProps);
+        schedulerStreams.setUncaughtExceptionHandler(ex -> {
+            logger.error("Kafka-Streams uncaught exception occurred. Stream will be replaced with new thread", ex);
+            return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD;
+        });
+
         if(config.isCleanUp()) {
             schedulerStreams.cleanUp();
         }
